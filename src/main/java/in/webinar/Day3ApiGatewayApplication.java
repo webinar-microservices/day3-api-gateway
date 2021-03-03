f0bade71a45970c8c3627781edf03319da51180a
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-03 14:22:48 +0530

    api.gateway.

---
+++ src/main/java/in/webinar/Day3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package in.webinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ApiGatewayApplication.class, args);
+	}
+
+}
